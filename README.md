ebd75f98bdfbbb57b43cd752d61055537e98beaf
Yuto Morohoshi <nnnakara@gmail.com>
2025-01-17 22:38:43 +0000

    create get_param.ipynb

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # fourier_learning_ibm
+
+## What's this?
+This is a code to reproduce the results of arxiv:hogehoge.
+
+## How to use
+1. Determine parameters
+
+Initially, we determine two parameters, specifically the Trotter step $n_{\text{step}}$ and the maximum evolution time $t_K$. This process is carried out using the [get_param.ipynb](get_param.ipynb) notebook.
--- heisenberg.py
+++ heisenberg.py
@@ -20,7 +20,7 @@ def get_n_steps(t):
     return n_steps
 
 
-def get_graph(n_qubits, rng, graph_type="tree"):
+def get_graph(n_qubits, Js):
     # グラフを作成
     G = nx.Graph()
 
@@ -29,128 +29,69 @@ def get_graph(n_qubits, rng, graph_type="tree"):
     G.add_nodes_from(nodes)
 
     # ノードに 'hadamard' 属性を追加
-    if graph_type == "tree":
-        print("ToDo: hadamard")
-    elif graph_type == "line":
-        for node in nodes:
-            if node == n_qubits // 2 - 1:
-                G.nodes[node]["hadamard"] = True
-            else:
-                G.nodes[node]["hadamard"] = False
+    for node in nodes:
+        if node == n_qubits // 2 - 1:
+            G.nodes[node]["hadamard"] = True
+        else:
+            G.nodes[node]["hadamard"] = False
 
     # エッジを追加
-    if graph_type == "tree":
-        if n_qubits == 10:
-            edges = [
-                (0, 1),
-                (0, 3),
-                (0, 4),
-                (1, 2),
-                (2, 8),
-                (2, 9),
-                (4, 5),
-                (5, 6),
-                (5, 7),
-            ]
-        elif n_qubits == 8:
-            edges = [(0, 1), (0, 3), (0, 4), (1, 2), (2, 7), (3, 5), (5, 6)]
-        elif n_qubits == 4:
-            edges = [(0, 1), (0, 3), (1, 2)]
-    elif graph_type == "line":
-        edges = [(i, i + 1) for i in range(n_qubits - 1)]
-
+    edges = [(i, i + 1) for i in range(n_qubits - 1)]
     G.add_edges_from(edges)
 
-    # エッジにランダムな重み (J_{ij}) を追加
+    # エッジに相互作用強度 (J_{ij}) を追加
     for edge in G.edges:
-        G.edges[edge]["J"] = rng.uniform(-1, 1)
+        G.edges[edge]["J"] = Js[edge[0]]
+        # G.edges[edge]["J"] = rng.uniform(-1, 1)
         # G.edges[edge]["J"] = rng.uniform(
         #     -1 / (3 * (n_qubits - 1)), 1 / (3 * (n_qubits - 1))
         # )
 
     # エッジに 'cnot' 属性を追加
-    if graph_type == "tree":
-        print("ToDo: cnot_order")
-    elif graph_type == "line":
-        # State |0011...1100> (center qubits are 1 and the rest are 0)
-        leftmost = n_qubits // 4
-        rightmost = leftmost + n_qubits // 2 - 1
-        hadamard = list(filter(lambda x: G.nodes[x]["hadamard"], G.nodes))[0]
-        left_start = hadamard
-        right_start = hadamard
+    # State |0011...1100> (center qubits are 1 and the rest are 0)
+    leftmost = n_qubits // 4
+    rightmost = leftmost + n_qubits // 2 - 1
+    hadamard = list(filter(lambda x: G.nodes[x]["hadamard"], G.nodes))[0]
+    left_start = hadamard
+    right_start = hadamard
 
-        for i in range(hadamard - leftmost + 1):
-            if i == 0:
-                # Hadamard より右側の処理
-                control = hadamard
-                target = hadamard + 1
+    for i in range(hadamard - leftmost + 1):
+        if i == 0:
+            # Hadamard より右側の処理
+            control = hadamard
+            target = hadamard + 1
+            right_start = target
+            G.edges[(control, target)]["cnot"] = {
+                "order": i,
+                "control": control,
+                "target": target,
+            }
+        else:
+            # Hadamard より左側の処理
+            control = left_start
+            target = left_start - 1
+
+            # target が leftmost を超えていない場合のみ CNOT を作用させる
+            if target >= leftmost:
+                G.edges[(control, target)]["cnot"] = {
+                    "order": i,
+                    "control": control,
+                    "target": target,
+                }
+                left_start = target
+
+            # Hadamard より右側の処理
+            control = right_start
+            target = right_start + 1
+
+            # target が rightmost を超えていない場合のみ CNOT を作用させる
+            if target <= rightmost:
+                G.edges[(control, target)]["cnot"] = {
+                    "order": i,
+                    "control": control,
+                    "target": target,
+                }
                 right_start = target
-                G.edges[(control, target)]["cnot"] = {
-                    "order": i,
-                    "control": control,
-                    "target": target,
-                }
-            else:
-                # Hadamard より左側の処理
-                control = left_start
-                target = left_start - 1
-
-                # target が leftmost を超えていない場合のみ CNOT を作用させる
-                if target >= leftmost:
-                    G.edges[(control, target)]["cnot"] = {
-                        "order": i,
-                        "control": control,
-                        "target": target,
-                    }
-                    left_start = target
-
-                # Hadamard より右側の処理
-                control = right_start
-                target = right_start + 1
-
-                # target が rightmost を超えていない場合のみ CNOT を作用させる
-                if target <= rightmost:
-                    G.edges[(control, target)]["cnot"] = {
-                        "order": i,
-                        "control": control,
-                        "target": target,
-                    }
-                    right_start = target
-
-        """
-        # State |111000> 
-        rightmost = n_qubits // 2 - 1
-        for i in range(math.ceil(n_qubits / 4)):
-            if i == 0:
-                if n_qubits == 4:
-                    control = 1
-                    target = 0
-                else:
-                    control = math.ceil(n_qubits / 4)
-                    target = math.ceil(n_qubits / 4) - 1
-                G.edges[(control, target)]["cnot"] = {
-                    "order": i,
-                    "control": control,
-                    "target": target,
-                }
-            else:
-                left_control = math.ceil(n_qubits / 4) - i
-                left_target = math.ceil(n_qubits / 4) - i - 1
-                G.edges[(left_control, left_target)]["cnot"] = {
-                    "order": i,
-                    "control": left_control,
-                    "target": left_target,
-                }
-                # CNOT を作用させる右ノードが、全体の左半分に収まっていれば、左半分のノードに対しても CNOT を作用させる
-                if math.ceil(n_qubits / 4) + i <= rightmost:
-                    right_control = math.ceil(n_qubits / 4) + i - 1
-                    right_target = math.ceil(n_qubits / 4) + i
-                    G.edges[(right_control, right_target)]["cnot"] = {
-                        "order": i,
-                        "control": right_control,
-                        "target": right_target,
-                    }
-        """
 
         # それ以外のエッジには None の 'cnot_order' 属性を追加
         for edge in G.edges:
@@ -160,68 +101,16 @@ def get_graph(n_qubits, rng, graph_type="tree"):
     return G
 
 
-def get_positions(n_qubits, graph_type):
+def get_positions(n_qubits):
     # 等間隔に配置するためのカスタム座標を定義
-    if graph_type == "tree":
-        if n_qubits == 10:
-            positions = {
-                0: (3, 1),
-                1: (4, 1),
-                2: (5, 1),
-                3: (3, 2),
-                4: (2, 1),
-                5: (1, 1),
-                6: (0, 1),
-                7: (1, 0),
-                8: (5, 0),
-                9: (6, 1),
-            }
-        elif n_qubits == 8:
-            positions = {
-                0: (3, 0),
-                1: (4, 0),
-                2: (5, 0),
-                3: (2, 0),
-                4: (3, 1),
-                5: (1, 0),
-                6: (0, 0),
-                7: (6, 0),
-            }
-        elif n_qubits == 4:
-            positions = {
-                0: (0, 0),
-                1: (1, 0),
-                2: (2, 0),
-                3: (0, 1),
-            }
-    elif graph_type == "line":
-        positions = {i: (i, 0) for i in range(n_qubits)}
+    positions = {i: (i, 0) for i in range(n_qubits)}
 
     return positions
 
 
-def get_initial_layout(n_qubits, graph_type, qpu_name):
-    if graph_type == "tree":
-        if qpu_name == "ibm_brisbane":
-            if n_qubits == 10:
-                initial_layout = [60, 61, 62, 53, 59, 58, 57, 71, 72, 63]
-            elif n_qubits == 8:
-                initial_layout = [60, 61, 62, 59, 53, 58, 57, 63]
-            elif n_qubits == 4:
-                initial_layout = [60, 61, 62, 53]
-        elif qpu_name == "ibm_torino":
-            if n_qubits == 10:
-                initial_layout = [103, 104, 105, 93, 102, 101, 100, 111, 112, 106]
-        elif qpu_name == "ibm_marrakesh":
-            if n_qubits == 10:
-                initial_layout = [103, 104, 105, 96, 102, 101, 100, 116, 117, 106]
-            elif n_qubits == 8:
-                initial_layout = [103, 104, 105, 102, 96, 101, 100, 106]
-    elif graph_type == "line":
-        # initial_layout = list(range(n_qubits))
-        if qpu_name == "ibm_marrakesh":
-            initial_layout = list(range(140, 140 + n_qubits))
-
+def get_initial_layout(n_qubits, qpu_name):
+    if qpu_name == "ibm_marrakesh":
+        initial_layout = list(range(140, 140 + n_qubits))
     return initial_layout
 
 
@@ -231,7 +120,7 @@ def get_prob0(result, n_qubits, mit=None):
     prob0_mit = 0
 
     if "0" * n_qubits not in meas_counts:
-        print(" > No counts for |0...0> state")
+        # print(" > No counts for |0...0> state")
         prob0_nmit = 0
     else:
         prob0_nmit = meas_counts["0" * n_qubits] / num_shots
@@ -392,15 +281,6 @@ class HeisenbergModel:
 
         U = scipy.sparse.linalg.expm(-1j * self.H.to_matrix(sparse=True) * t)
 
-        # 有望
-        # H = Operator(self.H).reverse_qargs().to_matrix()
-        # H = scipy.sparse.csr_matrix(H)
-        # U = scipy.sparse.linalg.expm(-1j * H * t)
-
-        # ダメ
-        # H = SparsePauliOp.from_operator(Operator(self.H).reverse_qargs())
-        # U = PauliEvolutionGate(H, t)
-
         ghz_circuit = self.get_ghz_circuit(phase=0)
         ghz_op = Operator(ghz_circuit)
 
@@ -412,10 +292,12 @@ class HeisenbergModel:
             ghz_op_with_phase.adjoint().data @ U @ ghz_op.data
         )
 
-        # "00...00" がなければ 0 を返す
-        prob0 = final_state.probabilities_dict().get("0" * self.n_qubits, 0)
+        # # "00...00" がなければ 0 を返す
+        # prob0 = final_state.probabilities_dict().get("0" * self.n_qubits, 0)
 
-        return prob0
+        # return prob0
+
+        return final_state
 
     def get_trotter_simulation_circuit(
         self, total_time, n_steps, phase=0, initial_layout=None
